afc38332206408defd0a1609d8f90ee462231590
ModdexEk <moddex@yandex.ru>
2024-10-06 20:05:29 +0500

    5666

--- index.js
+++ index.js
@@ -1,51 +1,97 @@
+const yargs = require("yargs");
 const path = require("path");
 const util = require("util");
 const fs = require("fs");
 
-const readdir = util.promisify(fs.readdir);
+const args = yargs
+      .usage('Usage: node $0 [options]')
+      .version('0.0.1')
+      .alias('version', 'v')
+      .help('help')
+      .alias('help', 'h')
+      .example('node $0 --entry ./path --dist ./path --delete')
+      .option('entry', {
+        alias: 'e',
+        describe: 'Указать путь к исходной директории',
+        demandOption: true
+      })
+      .option('dist', {
+        alias: 'd',
+        describe: 'Указать путь к dist директории',
+        default: './dist'
+      })
+      .option('delete', {
+        alias: 'D',
+        describe: 'Удалять ли исходную папку',
+        boolean: true,
+        default: false
+      })
+      .epilog('Первая домашка')
+      .argv
 
 const config = {
-  typeDirs: [
-    { type: ".pdf", directory: "documents" },
-    { type: ".png", directory: "images" },
-    { type: ".mp3", directory: "music" }
-  ]
-};
-
-const directory = process.argv[2];
-
-if (!directory) {
-  console.log("Specify target directory");
-  return;
+    entry: path.normalize(path.join(__dirname, args.entry)),
+    dist: path.normalize(path.join(__dirname, args.dist)),
+    delete: args.delete
 }
 
-[...config.typeDirs, { directory: "other" }].map(d => {
-  const dirname = `${directory}/${d.directory}`;
-  if (!fs.existsSync(dirname)) {
-    fs.mkdirSync(dirname);
-  }
-});
+function createDir(src, callback) {
+    fs.mkdir(src, (err) => {
+        if (err && err.code === 'EEXIST') {
+            callback(null)
+        } else if (err) {
+            callback(err)
+        } else {
+            callback(null)
+        }
+    })
+}
 
-(async () => {
-  const files = await readdir(directory);
+function sorter (src) {
+    fs.readdir(src, (err, files) => {
+        if (err) throw err
 
-  files.forEach(file => {
-    const extname = path.extname(file);
-    if (!extname) {
-      return;
-    }
+        files.forEach((file) => {
+            const currentPath = path.join(src, file)
 
-    const { directory: targetDir = "other" } = config.typeDirs.find(
-      dir => dir.type == extname
-    ) || {};
+            fs.stat(currentPath, (err, stat) => {
+                if (err) throw err
 
-    const oldPath = path.join(__dirname, directory, file);
-    const newPath = path.join(__dirname, directory, targetDir, file);
+                if (stat.isDirectory()) {
+                    sorter(currentPath)
+                } else {
+                    createDir(config.dist, (err) => {
+                        if (err) throw err
 
-    fs.rename(oldPath, newPath, function(err) {
-      if (err) {
-        throw err;
-      }
-    });
-  });
-})();
+                        const folderName = path.join(config.dist, file[0].toUpperCase())
+
+                        createDir(folderName, (err) => {
+                            if (err) throw err
+
+                            console.log('тест', currentPath)
+                            const newPath = path.join(folderName, file)
+                            fs.rename(currentPath, newPath, err => {
+                                if (err) {
+                                  console.error(err.message);
+                                  return;
+                                }
+                              })
+
+                            
+                        })
+                    })
+
+                    
+                }
+
+            })
+
+        })
+    })
+}
+
+try {
+    sorter(config.entry)
+} catch (error) {
+    console.error(error)
+}
